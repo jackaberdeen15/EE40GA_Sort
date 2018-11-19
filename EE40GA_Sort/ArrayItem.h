8dc8b4cac762453ea77e95b2f5a62929ce15a80b
jackaberdeen15 <43541345+jackaberdeen15@users.noreply.github.com>
2018-11-19 11:14:33 +0000

    Added integer_item and neg_int_item

--- EE40GA_Sort/ArrayItem.h
+++ EE40GA_Sort/ArrayItem.h
@@ -78,6 +78,238 @@ public:
 
 };
 
+// Example of derived class implementing an integer item
+class integer_item : public basic_item {
+protected:
+	int item_value;
+
+public:
+	// default constructor and destructor: do nothing
+	integer_item() { ; }
+	~integer_item() { ; }
+
+	int getItemVal() { return item_value; }
+
+	virtual void printItemOnScreen()
+	{
+		if (isEmpty())
+			cout << "Item is empty." << endl;
+		else
+			cout << "Item value is " << getItemVal() << " . " << endl;
+	}
+
+	virtual void enterItemFromKeyboard()
+	{
+		cout << "Insert element then hit enter." << endl;
+		cin >> item_value;
+		cout << endl;
+
+		// item filled
+		empty = false;
+	}
+
+	virtual void generateRandomItem()
+	{
+		int item;
+		int max_rand_val = 1000;
+
+		item = rand();
+		item = item % (max_rand_val + 1);
+
+		// turn to negative 50% of the time
+		if ((rand() % 10) >= 5)
+			item = (-1)*item;
+
+		item_value = item;
+		// item filled
+		empty = false;
+
+	}
+
+	//virtual void loadItemFromFile(FILE* fin);
+
+	virtual bool IsLargerThan(basic_item* other_item, basic_sort_criteria* sort_criteria = NULL)
+	{
+		bool result = false;
+		// if the other item is "empty" (non allocated) don't do any comparison
+		if (other_item == NULL)
+			return false;
+
+		// first typecast the other item to confimr it is the same as this;
+		integer_item* typecasted_other_item = typecastItem(other_item, this);
+
+		// check that it worked
+		if (typecasted_other_item == NULL)
+		{
+			cout << "Other item is not of type integer_item." << endl;
+			return false;
+			// items of the wrong type (or null pointers) will be pushed to the end of the list
+		}
+
+		// now verify if the other item is larger than the curren
+		if (getItemVal() > (typecasted_other_item->getItemVal()))
+			result = true;
+
+
+		// chek if there are sorting options to apply 
+		if (sort_criteria != NULL)
+		{
+			// if sorting is in descending order the result is reversed 
+			if (!(sort_criteria->getAscending()))
+				result = !result;
+		}
+
+		return result;
+	}
+
+	virtual bool IsSmallerThan(basic_item* other_item, basic_sort_criteria* sort_criteria = NULL)
+	{
+		bool result = false;
+		// if the other item is "empty" (non allocated) don't do any comparison
+		if (other_item == NULL)
+			return false;
+
+		// first typecast the other item to confimr it is the same as this;
+		integer_item* typecasted_other_item = typecastItem(other_item, this);
+
+		// check that it worked
+		if (typecasted_other_item == NULL)
+		{
+			cout << "Other item is not of type height_item." << endl;
+			return false;
+			// items of the wrong type (or null pointers) will be pushed to the end of the list
+		}
+
+		// now verify if the other item is larger than the curren
+		if (getItemVal() < (typecasted_other_item->getItemVal()))
+			result = true;
+
+
+		// chek if there are sorting options to apply
+		if (sort_criteria != NULL)
+		{
+			// if sorting is in descending order the result is reversed
+			if (!(sort_criteria->getAscending()))
+				result = !result;
+		}
+
+		return result;
+	}
+
+	virtual bool IsEqualTo(basic_item* other_item, basic_sort_criteria* sort_criteria = NULL)
+	{
+		bool result = false;
+		// if the other item is "empty" (non allocated) don't do any comparison
+		if (other_item == NULL)
+			return false;
+
+		// first typecast the other item to confimr it is the same as this;
+		integer_item* typecasted_other_item = typecastItem(other_item, this);
+
+		// check that it worked
+		if (typecasted_other_item == NULL)
+		{
+			cout << "Other item is not of type height_item." << endl;
+			return false;
+			// items of the wrong type (or null pointers) will be pushed to the end of the list
+		}
+
+		// now verify if the other item is larger than the curren
+		if (getItemVal() == (typecasted_other_item->getItemVal()))
+			result = true;
+
+
+		// chek if there are sorting options to apply 
+		if (sort_criteria != NULL)
+		{
+			// if sorting is in descending order the result is reversed 
+			if (!(sort_criteria->getAscending()))
+				result = !result;
+		}
+
+		return result;
+	}
+
+	virtual void deallocateItem(basic_item* item_ptr)
+	{
+		// first typecast the other item to confimr it is the same as this;
+		integer_item* typecasted_item_ptr = typecastItem(item_ptr, this);
+
+		// check that it worked
+		if (typecasted_item_ptr == NULL)
+		{
+			// items of the wrong type (or null pointers)
+			cout << "Error in deallocateItem (for integer_item): " << endl << "Other item is not of type integer_item." << endl;
+			return;
+		}
+		delete typecasted_item_ptr;
+	}
+	virtual basic_item* allocateItem()
+	{
+		integer_item* result = new integer_item;
+		if (result == NULL)
+			cout << " Error in integer_item::allocateItem(): out of memory" << endl;
+		return result;
+	}
+};
+
+
+// This object only accepts negative elements; it is readily derived from integer_item 
+// by implementing only a few functions (all others are inhereited from the parent).
+class neg_int_item : public integer_item {
+public:
+	// We must implement the following two functions to allocate/free neg_int_item differently from integer_item
+	virtual void deallocateItem(basic_item* item_ptr)
+	{
+		// first typecast the other item to confimr it is the same as this;
+		neg_int_item* typecasted_item_ptr = typecastItem(item_ptr, this);
+
+		// check that it worked
+		if (typecasted_item_ptr == NULL)
+		{
+			// items of the wrong type (or null pointers)
+			cout << "Error in deallocateItem (for neg_int_item): " << endl << "Other item is not of type integer_item." << endl;
+			return;
+		}
+		delete typecasted_item_ptr;
+	}
+	virtual basic_item* allocateItem()
+	{
+		neg_int_item* result = new neg_int_item;
+		if (result == NULL)
+			cout << " Error in neg_int_item::allocateItem(): out of memory" << endl;
+		return result;
+	}
+	//---------------------------------------------------------------------------------------//
+	// We implement the following two functions to fill neg_int_item differently from integer_item
+	virtual void enterItemFromKeyboard()
+	{
+		//warn the user:
+		cout << "Insertion of neg_int_item: element value must be negative." << endl;
+		// loop till you get a negative input number
+		while (empty)
+		{
+			//call the parent funciton to fill the object
+			integer_item::enterItemFromKeyboard();
+			if (getItemVal() >= 0)
+				empty = true;
+		}
+	}
+	virtual void generateRandomItem()
+	{
+		// loop till you get a negative random number
+		while (empty)
+		{
+			//call the parent funciton to fill the object
+			integer_item::generateRandomItem();
+			if (getItemVal() >= 0)
+				empty = true;
+		}
+	}
+
+};
+
+
 // height item child
 class height_item: public basic_item{
 protected:
--- EE40GA_Sort/Test_file.cpp
+++ EE40GA_Sort/Test_file.cpp
@@ -1,13 +1,47 @@
 #include "ArrayItem.h"
+#include "generalArray.h"
 
+
+void test_array(int array_size, basic_item* ref_item)
+{
+	basic_sort_criteria srt_crt(true);
+	general_array test_array;
+	// attach the "integer item" as referecen item
+	test_array.attachRefrenceItem(ref_item);
+	// now allocate the array	
+	test_array.allocateArray(array_size);
+	// print content to screen (empty)
+	cout << " Array is empty at this time." << endl << endl;
+	test_array.printArrayOnScreen();
+	// fill with random items
+	cout << endl << " Filling Array with random values." << endl;
+	test_array.fillRandomValueArray();
+	// print content to screen
+	test_array.printArrayOnScreen();
+	// sort the array 
+	cout << endl << " Sorting Array." << endl;
+	test_array.bubblesort(&srt_crt);
+	test_array.printArrayOnScreen();
+	cout << endl;
+}
 
 int main2()
 {
-	student_record dob;
-	
-
-	dob.generateRandomItem();
-	dob.printItemOnScreen();
-
+	int array_size = 10;
+	int temp;
+	// Used as "example item" by the general_array to populate the entire array with integer_item 
+	integer_item ref_int_item;
+	// Used as "example item" by the general_array to populate the entire array with neg_int_item 
+	neg_int_item ref_nint_item;
+	// First test with the inger item
+	cout << " Testing array with integer (positve and negative) items: " << endl << endl;
+	test_array(array_size, &ref_int_item);
+	cout << " Done. Enter any nymber to progress to the next test." << endl;
+	cin >> temp;
+	// Next test with the negative ingteger item
+	cout << " Testing array with NEGATIVE ONLY integer items: " << endl << endl;
+	test_array(array_size, &ref_nint_item);
+	cout << " Done. Enter any nymber to finish." << endl;
+	cin >> temp;
 	return 0;
 }
